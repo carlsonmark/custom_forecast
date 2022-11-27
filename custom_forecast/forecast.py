ecb6dca16dbca60e1f4321a81b035505b5b56a28
carlsonmark <carlsonmark@gmail.com>
2022-11-27 12:52:27 -0700

    Moved all of the pandas code to forecast.py. Next step, adding caching.

--- custom_forecast/forecast.py
+++ custom_forecast/forecast.py
@@ -3,7 +3,11 @@ import urllib.parse
 import urllib.request
 from html.parser import HTMLParser
 
+import numpy as np
+import pandas as pd
 from pydap.client import open_url
+from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.signal import savgol_filter
 
 
 class DDSParser(HTMLParser):
@@ -68,3 +72,50 @@ def get_pressure(lat, lon, url, dataset=None):
     t_list = t.data.reshape((t.data.size, )) - 1
     # Return the pressures and times, along with the lat/long from the forecast
     return p_list, t_list, lat, lon
+
+
+def pressure_derivative(pressure: pd.array,
+                        derivative_scale: float = 20) -> pd.array:
+    x = np.arange(len(pressure))
+    filtered_pressure = savgol_filter(pressure, len(pressure) // 8, 3)
+    f = InterpolatedUnivariateSpline(x, filtered_pressure, k=1)
+    derivative = f.derivative()(x) * derivative_scale
+    return derivative
+
+
+def latest_data_frames():
+    urls = get_urls(-5, None)
+    data_frames = []
+    # Load data from files for testing, since the server has low limits on how
+    # many times you can use it before it blocks you.
+    load_data = True
+    if load_data:
+        file_counter = 0
+        file_found = True
+        while file_found:
+            try:
+                df = pd.read_pickle(f'data-{file_counter}.pkl')
+                df['derivative'] = pressure_derivative(df['pressure'])
+                data_frames.append(df)
+                file_counter += 1
+            except FileNotFoundError as e:
+                file_found = False
+    else:
+        for i, url in enumerate(urls):
+            pressure, time_, lat, lon = get_pressure(51.05, 114.0677, url)
+            # It seems like the time offset is not needed now?
+            # time_offset = (-time.timezone) / (24.0 * 3600.0)
+            # time_with_offset = time_ + time_offset
+            ordinal_date = time_.astype(int)
+            fractional_date = time_ - ordinal_date
+            seconds = (fractional_date * (24 * 60 * 60)).astype(int)
+            timestamp = pd.array(
+                [pd.Timestamp.fromordinal(d) for d in ordinal_date])
+            timedelta = pd.array([pd.Timedelta(seconds=s) for s in seconds])
+            df = pd.DataFrame(
+                dict(time=timestamp + timedelta,
+                     pressure=pressure,
+                     derivative=pressure_derivative(pressure)))
+            df.to_pickle(f'data-{i}.pkl')
+            data_frames.append(df)
+    return data_frames
